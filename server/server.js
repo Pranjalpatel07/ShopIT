79977e475cf6bb8109364f61ff8a4fa45f014e42
Pranjal patel <prannjalpatel8827@gmail.com>
2026-06-03 22:17:47 +0530

    updating cors policy

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin:  "http://localhost:5173",
+    origin:  process.env.CLIENT_URL || "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
